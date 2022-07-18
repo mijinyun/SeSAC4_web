d0435eff32765caa623ae336487c09f22383a56b
mijinyun <yunmj1224@gmail.com>
2022-07-18 19:13:08 +0900

    study:nodejs_promise.js 수정

---
+++ nodejs/220718/promise.js
@@ -0,0 +1,13 @@
+const func1 = new Promise((resolve, reject) => {
+    var flag = true;
+    if (flag) resolve ("성공");
+    else reject ("실패");
+})
+
+func1.then( value => {
+    return value + "1";
+}).then(result => {
+    console.log(result);
+}).catch(err => {
+    console.log(err);
+});
