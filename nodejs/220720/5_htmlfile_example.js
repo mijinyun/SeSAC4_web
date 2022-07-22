ed64449d52c0dd279db2244b59914eb74a9bf315
mijinyun <yunmj1224@gmail.com>
2022-07-22 14:23:09 +0900

    pull error (5_htmlfile_example.js 수정_다시할 예정)

--- nodejs/220720/5_htmlfile_example.js
+++ nodejs/220720/5_htmlfile_example.js
@@ -3,22 +3,14 @@ const fs = require ('fs').promises;
 
 // 27.96.135.21
 const port = 8080;
-<<<<<<< HEAD
 // const hostname = '27.96.135.21';
-=======
-const hostname = '0.0.0.0';
->>>>>>> 754c5cf27b1893287de24f72cfb8edc9f985b691
 const server = http.createServer(async function (req, res) {
 
 
     // C:\Users\yunmj\github\week1\larvar2_mod.html'
     try {
-<<<<<<< HEAD
         console.log(req.url);
         const data = await fs.readFile('./larva2_mod.html');
-=======
-        const data = await fs.readFile('../../week1/larva1.html');
->>>>>>> 754c5cf27b1893287de24f72cfb8edc9f985b691
         res.writeHead(200);
         res.end(data);
     }
@@ -29,7 +21,6 @@ const server = http.createServer(async function (req, res) {
     }
 });
 
-<<<<<<< HEAD
 server.listen(port,function() {
     console.log ("8080번 포트");
 })
@@ -37,9 +28,3 @@ server.listen(port,function() {
 
 
 ///내가 안되었던 이유는 컴퓨터는 노드가 16버전인데 푸티에서는 8버전이여서
-=======
-server.listen(port,hostname,'50000',function() {
-    console.log ("8080번 포트");
-})
-
->>>>>>> 754c5cf27b1893287de24f72cfb8edc9f985b691
