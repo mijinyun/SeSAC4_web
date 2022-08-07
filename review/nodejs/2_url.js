05ba2c35259a87e58dba049b8c8de987507dd89d
mijinyun <yunmj1224@gmail.com>
2022-08-07 22:48:52 +0900

    url모듈 추가공부

--- review/nodejs/2_url.js
+++ review/nodejs/2_url.js
@@ -1,17 +1,29 @@
 const url = require("url");
 const { URL } = url;  // const URL = url.URL; 이랑 같음. { URL }은 구조분해해서 가져오는 것
-
+const querystring = require("querystring");
 
 const string = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=Sesac";
 const naver = new URL(string);
+const parsedUrl = url.parse(string);
+const query = querystring.parse(parsedUrl.query);
 
-console.log(naver.searchParams);
-console.log(naver.searchParams.keys());
-console.log(naver.searchParams.values());
+// console.log(naver);
 
-naver.searchParams.delete('sm');
+// console.log(naver.searchParams);
+// console.log(naver.searchParams.keys());
+// console.log(naver.searchParams.values());
+// naver.searchParams.delete('sm');
+// console.log(naver.searchParams.keys());
 
-console.log(naver.searchParams.keys());
+// console.log(parsedUrl);
+console.log(query);
+
+
+
+
+
+
+
 
 
 // console.log(process.version);
